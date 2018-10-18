1b3f8a073cec00a8eff8e4d523c6ff1984945ac7
argot <pablotesari@gmail.com>
2018-10-18 17:06:43 -0300

    IntentService terminado

---
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/PrepararPedidoService.java
@@ -0,0 +1,43 @@
+package ar.edu.utn.frsf.dam.isi.laboratorio02;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import java.util.List;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+
+public class PrepararPedidoService extends IntentService {
+
+    public PrepararPedidoService() {
+        super("PrepararPedidoService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            // esperamos 20 seg
+            try {
+               Thread.sleep(20000);
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
+
+            // obtenemos lista de pedidos
+            List<Pedido> pedidos = new PedidoRepository().getLista();
+            // iteramos sobre pedidos cambiando los estados "ACEPTADO" a "EN_PREPARACION"
+            // y enviamos un broadcast notificando de dicho cambio
+            for (Pedido p:pedidos) {
+                if (!p.getEstado().equals(Pedido.Estado.ACEPTADO)) { continue; }
+
+                p.setEstado(Pedido.Estado.EN_PREPARACION);
+
+                Intent i = new Intent();
+                i.setAction(EstadoPedidoReceiver.ESTADO_EN_PREPARACION);
+                i.putExtra("idPedido", p.getId());
+                sendBroadcast(i);
+            }
+        }
+    }
+}
