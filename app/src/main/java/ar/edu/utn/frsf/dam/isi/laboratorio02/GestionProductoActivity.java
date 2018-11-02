7d40584170756c20fefbd7c388ecad5d427ad836
argot <pablotesari@gmail.com>
2018-11-01 21:08:37 -0300

    utilizar retrofit para gestionar productos

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/GestionProductoActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/GestionProductoActivity.java
@@ -2,12 +2,239 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRetrofit;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.CategoriaRest;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class GestionProductoActivity extends AppCompatActivity {
+    private ToggleButton tgGestionProdCrearProdNuevo;
+    private EditText edtGestionProdBuscarPorId;
+    private Button btnGestionProdBuscarPorId;
+    private EditText edtGestionProdNombre;
+    private EditText edtGestionProdDescripcion;
+    private EditText edtGestionProdPrecio;
+    private Spinner spGestionProdSpinner;
+    private Button btnGestionProdGuardar;
+    private Button btnGestionProdBorrar;
+    private Button btnGestionProdVolver;
+
+    private ArrayAdapter<Categoria> categoriaAdapter;
+
+    //private Categoria prod_categoria = null;
+    private int categoria_position = -1;
+    private Boolean actualizacion = false;
+    private int prod_id = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion_producto);
+
+        // setup spinner de productos
+        spGestionProdSpinner = (Spinner) findViewById(R.id.spGestionProdSpinner);
+
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                CategoriaRest catRest = new CategoriaRest();
+                final Categoria[] cats = catRest.listarTodas().toArray(new Categoria[0]);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        categoriaAdapter = new ArrayAdapter<Categoria>(GestionProductoActivity.this, android.R.layout.simple_spinner_dropdown_item, cats);
+                        spGestionProdSpinner.setAdapter(categoriaAdapter);
+                        spGestionProdSpinner.setSelection(0);
+                    }
+                });
+            }
+        };
+
+        spGestionProdSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //prod_categoria = (Categoria) parent.getItemAtPosition(position);
+                categoria_position = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Thread t = new Thread(r);
+        t.start();
+
+
+        // setup busqueda de producto por ID
+        edtGestionProdBuscarPorId = (EditText) findViewById(R.id.edtGestionProdBuscarPorId);
+        btnGestionProdBuscarPorId = (Button) findViewById(R.id.btnGestionProdBuscarPorId);
+        btnGestionProdBorrar = (Button) findViewById(R.id.btnGestionProdBorrar);
+        edtGestionProdNombre = (EditText) findViewById(R.id.edtGestionProdNombre);
+        edtGestionProdDescripcion = (EditText) findViewById(R.id.edtGestionProdDescripcion);
+        edtGestionProdPrecio = (EditText) findViewById(R.id.edtGestionProdPrecio);
+        btnGestionProdVolver = (Button) findViewById(R.id.btnGestionProdVolver);
+        tgGestionProdCrearProdNuevo = (ToggleButton) findViewById(R.id.tgGestionProdCrearProdNuevo);
+        btnGestionProdGuardar = (Button) findViewById(R.id.btnGestionProdGuardar);
+
+        tgGestionProdCrearProdNuevo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                actualizacion = isChecked;
+                edtGestionProdBuscarPorId.setEnabled(isChecked);
+                btnGestionProdBuscarPorId.setEnabled(isChecked);
+                btnGestionProdBorrar.setEnabled(isChecked);
+            }
+        });
+
+        btnGestionProdBuscarPorId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                prod_id = Integer.valueOf(edtGestionProdBuscarPorId.getText().toString());
+
+                ProductoRetrofit clienteRest = RestClient.getInstance()
+                        .getRetrofit()
+                        .create(ProductoRetrofit.class);
+
+                Call<Producto> busquedaCall = clienteRest.buscarProductoId(prod_id);
+
+                busquedaCall.enqueue(new Callback<Producto>() {
+                    @Override
+                    public void onResponse(Call<Producto> call, Response<Producto> response) {
+                        Producto producto = response.body();
+                        if (producto == null) {
+                            Log.e("LAB_04", "No existe producto con este id");
+                            return;
+                        }
+
+                        edtGestionProdNombre.setText(producto.getNombre());
+                        edtGestionProdDescripcion.setText(producto.getDescripcion());
+                        edtGestionProdPrecio.setText(String.valueOf(producto.getPrecio()));
+
+                        categoria_position = categoriaAdapter.getPosition(producto.getCategoria());
+                        spGestionProdSpinner.setSelection(categoria_position);
+                    }
+
+                    @Override
+                    public void onFailure(Call<Producto> call, Throwable t) {
+                        Log.e("LAB_04", call.toString());
+                        Log.e("LAB_04", t.toString());
+                    }
+                });
+            }
+        });
+
+        // guardar producto
+        btnGestionProdGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String prod_nombre = edtGestionProdNombre.getText().toString();
+                Log.d("LAB_04", prod_nombre);
+                if (prod_nombre.equals("")) { return; }
+
+                String prod_descripcion = edtGestionProdDescripcion.getText().toString();
+                Log.d("LAB_04", prod_descripcion);
+                if (prod_descripcion.equals("")) { return; }
+
+                Double prod_precio = Double.valueOf(edtGestionProdPrecio.getText().toString());
+                Log.d("LAB_04", String.valueOf(prod_precio));
+                if (prod_precio < 0.0) { return; }
+
+                Categoria prod_categoria = (Categoria) spGestionProdSpinner.getItemAtPosition(categoria_position);
+
+                Producto p = new Producto(prod_nombre, prod_descripcion, prod_precio, prod_categoria);
+
+                ProductoRetrofit clienteRest = RestClient.getInstance()
+                        .getRetrofit()
+                        .create(ProductoRetrofit.class);
+
+                if (actualizacion) {
+
+                    Call<Producto> actualizacionCall = clienteRest.actualizarProducto(prod_id, p);
+
+                    actualizacionCall.enqueue(new Callback<Producto>() {
+                        @Override
+                        public void onResponse(Call<Producto> call, Response<Producto> response) {
+                            Log.d("LAB_04", call.toString());
+                            Log.d("LAB_04", response.toString());
+                        }
+
+                        @Override
+                        public void onFailure(Call<Producto> call, Throwable t) {
+                            Log.e("LAB_04", call.toString());
+                            Log.e("LAB_04", t.toString());
+                        }
+                    });
+
+                } else {
+
+                    Call<Producto> altaCall = clienteRest.crearProducto(p);
+
+                    altaCall.enqueue(new Callback<Producto>() {
+                        @Override
+                        public void onResponse(Call<Producto> call, Response<Producto> response) {
+                            Log.d("LAB_04", call.toString());
+                            Log.d("LAB_04", response.toString());
+                        }
+
+                        @Override
+                        public void onFailure(Call<Producto> call, Throwable t) {
+                            Log.e("LAB_04", call.toString());
+                            Log.e("LAB_04", t.toString());
+                        }
+                    });
+                }
+            }
+        });
+
+        // borrar producto
+        btnGestionProdBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProductoRetrofit clienteRest = RestClient.getInstance()
+                        .getRetrofit()
+                        .create(ProductoRetrofit.class);
+
+                Call<Producto> borrarCall = clienteRest.borrar(prod_id);
+
+                borrarCall.enqueue(new Callback<Producto>() {
+                    @Override
+                    public void onResponse(Call<Producto> call, Response<Producto> response) {
+                        Log.d("LAB_04", call.toString());
+                        Log.d("LAB_04", response.toString());
+                    }
+
+                    @Override
+                    public void onFailure(Call<Producto> call, Throwable t) {
+                        Log.e("LAB_04", call.toString());
+                        Log.e("LAB_04", t.toString());
+                    }
+                });
+            }
+        });
+
+        // volver
+        btnGestionProdVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProductos.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProductos.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,24 +14,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import ar.edu.utn.frsf.dam.isi.laboratorio02.adapter.ProductoAdapter;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRetrofit;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.decoration.DividerItemDecoration;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.decoration.VerticalSpaceItemDecoration;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.CategoriaRest;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ListaProductos extends AppCompatActivity {
 
     static final int LISTAPRODUCTO_REQUEST = 9;
+    private List<Producto> productos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_productos);
-
-        final ProductoRepository productoRepo = new ProductoRepository();
-        int idProductoSel = 0;
 
         // esto será ejecutado en segundo plano
         Runnable r = new Runnable() {
@@ -38,6 +46,18 @@ public class ListaProductos extends AppCompatActivity {
             public void run() {
                 CategoriaRest catRest = new CategoriaRest();
                 final Categoria[] cats = catRest.listarTodas().toArray(new Categoria[0]);
+
+                ProductoRetrofit clienteRest = RestClient.getInstance()
+                        .getRetrofit()
+                        .create(ProductoRetrofit.class);
+
+                Call<List<Producto>> listarCall = clienteRest.listarProductos();
+
+                try {
+                    productos = listarCall.execute().body();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
                 runOnUiThread(new Runnable() {
                     @Override
@@ -47,12 +67,14 @@ public class ListaProductos extends AppCompatActivity {
                         cmbProductoCategoria.setAdapter(new ArrayAdapter<Categoria>(ListaProductos.this, android.R.layout.simple_spinner_dropdown_item, cats));
                         cmbProductoCategoria.setSelection(0);
 
-                        final ProductoAdapter productoAdapter = new ProductoAdapter(productoRepo.buscarPorCategoria(productoRepo.getCategorias().get(0)));
+                        //final ProductoAdapter productoAdapter = new ProductoAdapter(productoRepo.buscarPorCategoria(productoRepo.getCategorias().get(0)));
+                        final ProductoAdapter productoAdapter = new ProductoAdapter(buscarPorCategoria(productos, cats[0]));
 
                         cmbProductoCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                             @Override
                             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                                productoAdapter.setProductoList(productoRepo.buscarPorCategoria((Categoria) parent.getItemAtPosition(position)));
+                                //productoAdapter.setProductoList(productoRepo.buscarPorCategoria((Categoria) parent.getItemAtPosition(position)));
+                                productoAdapter.setProductoList(buscarPorCategoria(productos, cats[position]));
                             }
 
                             @Override
@@ -106,66 +128,13 @@ public class ListaProductos extends AppCompatActivity {
 
         Thread hiloCargarCombo = new Thread(r);
         hiloCargarCombo.start();
+    }
 
-        /*final ProductoRepository productoRespository = new ProductoRepository();
-
-        // widgets
-        // spinner con categorías de productos
-        final Spinner cmbProductosCategoria = (Spinner) findViewById(R.id.cmbProductosCategoria);
-        cmbProductosCategoria.setAdapter(new ArrayAdapter<Categoria>(ListaProductos.this, android.R.layout.simple_spinner_dropdown_item, productoRespository.getCategorias()));
-
-        // inicializar lista
-        final RecyclerView lstProductos = (RecyclerView) findViewById(R.id.lstProductos);
-        lstProductos.setHasFixedSize(true);
-        lstProductos.setLayoutManager(new LinearLayoutManager(this));
-        // decoración de la lista
-        lstProductos.addItemDecoration(new VerticalSpaceItemDecoration(48));
-        lstProductos.addItemDecoration(new DividerItemDecoration(this));
-        // setar adapter
-        final ProductoAdapter productoAdapter = new ProductoAdapter(productoRespository.buscarPorCategoria(productoRespository.getCategorias().get(0)));
-        lstProductos.setAdapter(productoAdapter);
-
-        // actualizar lista de productos al cambiar opción en spinner
-        cmbProductosCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                productoAdapter.setProductoList(productoRespository.buscarPorCategoria((Categoria) parent.getItemAtPosition(position)));
-                productoAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        final EditText edtProductoCantidad = (EditText) findViewById(R.id.edtProdCantidad);
-        final Button btnProdAddPedido = (Button) findViewById(R.id.btnProdAddPedido);
-
-        // chequear si la actividad padre genero un intent con un extra NUEVO_PEDIDO = 1
-        // si es true habilitar edittext, button y setear clicklistener
-        if (getIntent().getIntExtra("NUEVO_PEDIDO", 0) == 1) {
-            edtProductoCantidad.setEnabled(true);
-            btnProdAddPedido.setEnabled(true);
-
-            btnProdAddPedido.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Producto prod = productoAdapter.getItem();
-                    Intent output = new Intent();
-
-                    if (prod == null) {
-                        setResult(Activity.RESULT_CANCELED, output);
-
-                    } else {
-                        output.putExtra("idProducto", prod.getId());
-                        output.putExtra("cantidad", Integer.valueOf(edtProductoCantidad.getText().toString()));
-                        setResult(Activity.RESULT_OK, output);
-                    }
-
-                    finish();
-                }
-            });
-        }*/
+    private List<Producto> buscarPorCategoria (List<Producto> prod, Categoria c) {
+        List<Producto> resultado = new ArrayList<>();
+        for (Producto p:prod) {
+            if (p.getCategoria().getId().equals(c.getId())) resultado.add(p);
+        }
+        return resultado;
     }
 }
